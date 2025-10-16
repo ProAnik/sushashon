94f589f44b64468b2d367a427b67dbf2cd3698a5
Md. Tanvir Hasan Anik <md.tanvirhasananik@Mds-MacBook-Pro.local>
2025-10-17 05:36:36 +0600

    blog update

--- resources/js/Pages/Publishing.vue
+++ resources/js/Pages/Publishing.vue
@@ -7,6 +7,17 @@ import {Head} from "@inertiajs/vue3";
 <template>
     <Head title="প্রকাশনী"/>
     <MainLayout>
+
+        <h1 class='text-[30px] text-center text-white mt-10'> Inclusive Government: A Road To Nations Peace And Security </h1>
+        <div class="flex justify-center mt-4">
+        <a href="/download-pdf" class="mt-4 inline-block bg-blue-600 w-96 flex justify-center text-center text-white px-4 py-2 rounded hover:bg-blue-700 transition">
+            Download PDF
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+            </svg>
+        </a>
+        </div>
+
         <div class="w-full mt-10 h-[80vh] border rounded-lg overflow-hidden shadow">
             <iframe
                 src="/inclusive_gov.pdf"
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,11 @@ Route::get('/blog', [PageController::class, 'blog'])->name('blog');
 Route::get('/publish', [PageController::class, 'publishing'])->name('publishing');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 
+Route::get('/download-pdf', function () {
+    $file = public_path('/inclusive_gov.pdf');
+    return response()->download($file, 'inclusive_gov.pdf');
+});
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -20,5 +25,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
 
 require __DIR__.'/auth.php';
